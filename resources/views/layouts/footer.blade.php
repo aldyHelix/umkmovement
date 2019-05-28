c2f526285aaa617e54f166f6140b11577f9d4771
Aldy Satria Gumilar <pilusnarziz3@gmail.com>
2019-05-29 02:43:18 +0700

    Admin LTE dipasang

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 0.0.1
+    </div>
+    <div class="col-md-4 col-xs-12">
+        <span class="copyright">&copy; Website ini dibangun oleh <a href="http://Ar-society.com">Ar-society.com</a> 2019 <small>aldyHelix-Developer</small></span>
+    </div>
+</footer>
